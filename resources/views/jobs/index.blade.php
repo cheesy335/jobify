ed7ead73b7a39627d55af76a6c2342349dfb8579
cheesy335 <chandrashekharsahu@tutanota.com>
2026-01-10 14:58:54 +0530

    updated employers_table migration now seprate from users

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,5 +1,5 @@
 <x-app>
-        <div class="grid grid-cols-3 lg:grid-cols-2 gap-8 w-full mt-10">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 w-full mt-10">
             @foreach ($jobs as $job)
                 <x-card>{{ $job->title }}</x-card>
             @endforeach
